9e1f09900162ba390e8018a9f5327eab720005d2
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-04-09 11:04:05 +0100

    Add a display feature
    Added an event listener that toggles the "open" and "show" classes when
    clicking the cards

--- js/app.js
+++ js/app.js
@@ -47,3 +47,12 @@ $('.restart').click(function(){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function reveal(card){
+   card.toggleClass('open');
+   card.toggleClass('show');
+ }
+
+ cards.click(function(){
+   reveal($(this));
+ });
